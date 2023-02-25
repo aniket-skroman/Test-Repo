4c8ebe3959a1cdb9838edeea864a204b34fd2125
aniket-skroman <aniket.suryawanshi@skromanglobal.com>
2023-02-25 11:41:01 +0530

    reduce a time for query context

--- repositories/vehicle-repository.go
+++ repositories/vehicle-repository.go
@@ -571,7 +571,7 @@ func (db *vehiclerepository) DeleteBatteryTempData(batteryData []string) error {
 		}},
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	res, err := db.batteryTempConnection.DeleteMany(ctx, filter)
@@ -657,7 +657,7 @@ func (db *vehiclerepository) AddBatteryToMain(batteryData []models.BatteryHardwa
 	bulkOption := options.BulkWriteOptions{}
 	bulkOption.SetOrdered(true)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	res, err := db.batteryMainConnection.BulkWrite(ctx, operations)
@@ -700,7 +700,7 @@ func (db *vehiclerepository) UpdateBMSReporting(batteryData []string) error {
 	bulkOption := options.BulkWriteOptions{}
 	bulkOption.SetOrdered(true)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	res, err := db.batteryReportingConnection.BulkWrite(ctx, operations)
